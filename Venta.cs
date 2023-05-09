908ab940ec2c5d641446ad9678d02e4b7f93f297
CamiLeal <133057325+CamiLeal@users.noreply.github.com>
2023-05-09 20:37:58 -0300

    Add files via upload

---
+++ Venta.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Leal_Camila
+{
+    internal class Venta
+    {
+        private int id;
+        private string comentarios;
+        private int idUsuario;
+
+
+        //Constructores
+        public Venta()
+        {
+            this.id = 0;
+            this.comentarios = string.Empty;
+            this.idUsuario = 0;
+        }
+
+        public Venta(int id, string comentarios, int idUsuario)
+        {
+            this.id = id;
+            this.comentarios = comentarios;
+            this.idUsuario = idUsuario;
+        }
+    }
+}
